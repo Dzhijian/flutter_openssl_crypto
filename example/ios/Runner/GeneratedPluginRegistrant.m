17ddac9d5481db23d5ede885f7c9ee1c49acd2bc
James <646724452@qq.com>
2022-10-27 18:50:27 +0800

    first commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_openssl_crypto/FlutterOpensslCryptoPlugin.h>)
+#import <flutter_openssl_crypto/FlutterOpensslCryptoPlugin.h>
+#else
+@import flutter_openssl_crypto;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterOpensslCryptoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterOpensslCryptoPlugin"]];
+}
+
+@end
